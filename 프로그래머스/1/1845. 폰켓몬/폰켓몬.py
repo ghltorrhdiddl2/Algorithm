0c86f88bfac3c84a3e4284b66fbe3f738ae174d2
SooJi <113848956+ghltorrhdiddl2@users.noreply.github.com>
2025-03-08 17:37:44 +0900

    [level 1] Title: 폰켓몬, Time: 0.71 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,3 @@
 # 최대한 많은 종류의 폰켓몬을 포함해서 N/2마리를 선택하려
 def solution(nums):
-    max_num = len(nums)/2
-    if len(set(nums)) >= max_num:
-        return max_num
-    else:
-        return len(set(nums))
+    return min(len(set(nums)), len(nums)/2)
