330e03f573600706d85b126c03ad98b4e8d1992a
SooJi <113848956+ghltorrhdiddl2@users.noreply.github.com>
2025-07-07 16:36:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 3241.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,18 @@
+def solution(number, limit, power):
+    answer = []
+    def get_div(n):
+        div = []
+        for i in range(1,int(n**0.5)+1):
+            if n%i==0:
+                div.append(i)
+                if i!=n//i:
+                    div.append(n//i)
+        return sorted(div)
+    
+    for n in range(1,number+1):
+        if len(get_div(n)) > limit:
+            answer.append(power)
+        else:
+            answer.append(len(get_div(n)))
+
+    return sum(answer)
